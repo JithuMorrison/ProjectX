e7914adc2d6f76bc01133d8ee2f58bedf630a304
JithuMorrison <jithus2004@gmail.com>
2025-06-06 15:21:25 +0530

    modified maindash

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
 import { Login } from './login/login';
+import { Maindash } from './maindash/maindash';
 
 export const routes: Routes = [
-    {path: '', component: Login},
-    {path: 'dash', component: Dashboard}
+  { path: '', component: Login },
+  { path: 'projects', component: Dashboard },
+  { path: 'dash', component: Maindash },
 ];
--- src/app/maindash/maindash.ts
+++ src/app/maindash/maindash.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-maindash',
   imports: [],
   templateUrl: './maindash.html',
-  styleUrl: './maindash.css'
+  styleUrl: './maindash.css',
 })
 export class Maindash {
+  constructor(private router: Router) {}
 
+  openProject(val: string) {
+    this.router.navigate(['/' + val], { queryParams: { name: 'Jithu' } });
+  }
 }
