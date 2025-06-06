071976a95a9045da2181accc141056c281434f90
JithuMorrison <jithus2004@gmail.com>
2025-06-06 16:01:46 +0530

    modified register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
 import { Login } from './login/login';
 import { Maindash } from './maindash/maindash';
+import { Register } from './register/register';
 
 export const routes: Routes = [
   { path: '', component: Login },
   { path: 'projects', component: Dashboard },
   { path: 'dash', component: Maindash },
+  { path: 'register', component: Register },
 ];
--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,11 +1,91 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.html',
-  styleUrl: './register.css'
+  styleUrl: './register.css',
 })
 export class Register {
+  constructor(private router: Router) {}
 
+  http = inject(HttpClient);
+
+  title = 'loginpage';
+  email: string = '';
+  uname: string = '';
+  fname: string = '';
+  lname: string = '';
+  password: string = '';
+
+  passwordErrors = {
+    hasLowercase: true,
+    hasUppercase: true,
+    hasDigit: true,
+    minLength: true,
+  };
+
+  validatePassword(value: string) {
+    this.passwordErrors.hasLowercase = /[a-z]/.test(value);
+    this.passwordErrors.hasUppercase = /[A-Z]/.test(value);
+    this.passwordErrors.hasDigit = /\d/.test(value);
+    this.passwordErrors.minLength = value.length >= 8;
+  }
+
+  gotodash() {
+    this.router.navigate(['/dash'], { queryParams: { name: this.uname } });
+  }
+
+  onUsernameChange(value: string) {
+    this.uname = value;
+  }
+
+  onfirstnameChange(value: string) {
+    this.fname = value;
+  }
+
+  onPasswordChange(value: string) {
+    this.password = value;
+    this.validatePassword(value);
+  }
+
+  login(form: any) {
+    if (form.valid) {
+      if (
+        this.passwordErrors.hasLowercase &&
+        this.passwordErrors.hasUppercase &&
+        this.passwordErrors.hasDigit &&
+        this.passwordErrors.minLength
+      ) {
+        this.http
+          .post('http://localhost:8080/login', {
+            param: this.uname,
+            password: this.password,
+          })
+          .subscribe(
+            (response) => {
+              console.log('Login successful:', response);
+              if (response == true) {
+                alert('Login successful:');
+                this.gotodash();
+              } else {
+                alert('Login failed: Invalid credentials');
+              }
+            },
+            (error) => {
+              console.error('Login failed:', error);
+              alert('Login failed. Please try again.');
+            }
+          );
+      } else {
+        alert('Password does not meet the requirements');
+      }
+    } else {
+      alert('Form is invalid');
+    }
+  }
 }
