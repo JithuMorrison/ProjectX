77916c78db63508939f06d8089e15de9fbd30a19
JithuMorrison <jithus2004@gmail.com>
2025-06-07 17:16:46 +0530

    added updateuser

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Relog } from '../relog';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-profile',
@@ -10,15 +12,33 @@ import { Relog } from '../relog';
   styleUrl: './profile.css',
 })
 export class Profile {
+  constructor(private router: Router) {}
+
   editMode: boolean = false;
 
   relog = inject(Relog);
+
+  http = inject(HttpClient);
 
   toggleEdit() {
     this.editMode = !this.editMode;
   }
 
   save() {
-    alert('Profile saved successfully!');
+    this.http
+      .put(
+        'http://localhost:3000/updateUser/' + this.relog.userdetails.id(),
+        this.relog.userdetails
+      )
+      .subscribe((response) => {
+        alert(response);
+      });
+    this.toggleEdit();
+  }
+
+  goback() {
+    this.router.navigate(['/dash'], {
+      queryParams: { name: this.relog.userdetails.email() },
+    });
   }
 }
