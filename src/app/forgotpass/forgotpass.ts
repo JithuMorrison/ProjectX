e77c9e624c65ee2dffaa35fa101506ead5b432af
JithuMorrison <jithus2004@gmail.com>
2025-06-23 11:53:55 +0530

    added forgot pass

---
+++ src/app/forgotpass/forgotpass.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forgotpass',
+  imports: [],
+  templateUrl: './forgotpass.html',
+  styleUrl: './forgotpass.css'
+})
+export class Forgotpass {
+
+}
